c1bb29e9102fbec7799464fa73e8bb4aa6478a65
pabulson <paul.bulson@allscripts.com>
2015-10-25 17:18:48 -0400

    Added alternative solutions

--- run_Analysis.R
+++ run_Analysis.R
@@ -1,24 +1,20 @@
-install.packages("dplyr")
-library(dplyr)
 library(data.table)
 library(reshape2)
+library(tidyr)
 
 # read the activity labels
-setwd("C:/Users/pabulson/Documents/Data Science/John Hopkins/Getting and Cleaning Data/Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset")
 activityLabels <- read.table("activity_labels.txt", header=FALSE, na.strings="?")
 colnames(activityLabels) <- c("activityID","activityName")
 
 # read the training data
-setwd("C:/Users/pabulson/Documents/Data Science/John Hopkins/Getting and Cleaning Data/Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train")
-trainActivity <- read.table("y_train.txt", header=FALSE, na.strings="?")
-trainValues <- read.table("X_train.txt", header=FALSE, na.strings="?")
-trainSubject <- read.table("subject_train.txt", header=FALSE, na.strings="?")
+trainActivity <- read.table("train/y_train.txt", header=FALSE, na.strings="?")
+trainValues <- read.table("train/X_train.txt", header=FALSE, na.strings="?", nrows=7400, colClasses=c(rep("numeric", 561)))
+trainSubject <- read.table("train/subject_train.txt", header=FALSE, na.strings="?")
 
 # read the test data
-setwd("C:/Users/pabulson/Documents/Data Science/John Hopkins/Getting and Cleaning Data/Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test")
-testActivity <- read.table("y_test.txt", header=FALSE, na.strings="?")
-testValues <- read.table("X_test.txt", header=FALSE, na.strings="?")
-testSubject <- read.table("subject_test.txt", header=FALSE, na.strings="?")
+testActivity <- read.table("test/y_test.txt", header=FALSE, na.strings="?")
+testValues <- read.table("test/X_test.txt", header=FALSE, na.strings="?", nrows=3000, colClasses=c(rep("numeric", 561)))
+testSubject <- read.table("test/subject_test.txt", header=FALSE, na.strings="?")
 
 # merge the training and test sets to create one data set
 mergedValues <- rbind(trainValues,testValues)
@@ -26,7 +22,6 @@ mergedActivity <- rbind(trainActivity, testActivity)
 mergedSubject <- rbind(trainSubject, testSubject)
 
 # appropriately label the data set with descriptive variable names
-setwd("C:/Users/pabulson/Documents/Data Science/John Hopkins/Getting and Cleaning Data/Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset")
 headers <- read.table("features.txt", header=FALSE, na.strings="?")
 colnames(mergedSubject) <- "subject"
 colnames(mergedActivity) <- "activityID"
@@ -34,11 +29,11 @@ colnames(mergedValues) <- t(headers[2])
 
 # merge the three datasets into one
 completeDataSet <- cbind(mergedSubject, mergedActivity, mergedValues)
-        
+
 # uses descriptive activity names to name the activities in the data set
 completeDataSet <- merge(completeDataSet, activityLabels,by="activityID",sort=FALSE,all=FALSE)
 
-# extract only the measurements on the mean and standard deviations for each measurement
+# extract only the measurements on the mean and standard deviation for each measurement
 # subsetting via grep command
 extractData <- cbind(completeDataSet[2], completeDataSet[564], completeDataSet[,grep("std\\(\\)|mean\\(\\)",colnames(completeDataSet))])
 
@@ -47,6 +42,17 @@ extractData.dt <- data.table(extractData)
 tidyData <- melt(extractData.dt, id=c("subject","activityName"))
 averageBySubjectActivityMeasurement <- unique(tidyData[,list(average=ave(value)),by='subject,activityName,variable'])
 averageBySubjectActivityMeasurement <- averageBySubjectActivityMeasurement[with(averageBySubjectActivityMeasurement,order(subject,variable,activityName)),]
+averageBySubjectActivityMeasurement <- separate(data=averageBySubjectActivityMeasurement, col=variable, into=c("measure","function", "direction"))
 
 # write the data
-write.table(averageBySubjectActivityMeasurement, file="tidyDataSet.txt", row.name=FALSE)
+write.table(averageBySubjectActivityMeasurement, file="tidyDataSetWithMelt.txt", row.name=FALSE)
+
+
+# alternative dataset (without melting)
+# different people will interpret tidy differently
+tidyData2 <- extractData.dt
+alternative <- tidyData2[,lapply(.SD, mean), by=c("subject","activityName")]
+
+# write the data
+write.table(alternative, file="tidyDataSetWithoutMelt.txt", row.name=FALSE)
+
